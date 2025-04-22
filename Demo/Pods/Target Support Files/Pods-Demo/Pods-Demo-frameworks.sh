89b715e3a4d9288feff2883659989ed22a5a2931
jueduibishi <171013526@qq.com>
2025-04-22 09:33:24 +0800

    demo

--- Demo/Pods/Target Support Files/Pods-Demo/Pods-Demo-frameworks.sh
+++ Demo/Pods/Target Support Files/Pods-Demo/Pods-Demo-frameworks.sh
@@ -176,12 +176,10 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${PODS_ROOT}/MiniAdSDKipa/mini/MiniSDKFramework.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/KSAdSDK/KSAdSDK.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/MiniAdSDK/MiniSDKFramework.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${PODS_ROOT}/MiniAdSDKipa/mini/MiniSDKFramework.framework"
-  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/KSAdSDK/KSAdSDK.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/MiniAdSDK/MiniSDKFramework.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
